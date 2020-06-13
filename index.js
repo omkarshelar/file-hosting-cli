ca7d14cf4ebdb66a3e9910a8e07bddc5f8122d9f
Omkar Shelar <omkarshelar24@gmail.com>
2020-06-13 19:19:36 +0530

    Added code comments

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const fs = require('fs');
 const mime = require('mime');
 const { program } = require('commander');
 
-program.version('1.0.0');
+program.version('1.1.0');
 
 program
 	.requiredOption('-f, --file <path-to-file>', 'Expiry time of the object in minutes')
-	.option('-e, --expire <expire-minutes>', 'Expiry time of the object in minutes. Default is 5 minutes. Minimum 5 minutes', 5);
+	.option('-e, --expire <expire-minutes>', 'Expiry time of the object in minutes. Minimum 5 minutes', 5);
 
 program.on('--help', () => {
-	console.log('');
 	console.log('Example call:');
 	console.log('$ fha -f hello.txt');
 	console.log('$ fha -f hello.txt -e 30');
 });
 
+
+// If no arguments more arguments sent to application, display help.
 if (process.argv.length === 2) {
 	program.outputHelp();
 	process.exit(0);
 }
-
 program.parse(process.argv);
 
+// Check minimum time
 if (program.expire < 5) {
 	console.error("Expiry of objects should be more than 5 minutes");
 }
@@ -44,10 +45,13 @@ if (!process.env.FH_API_KEY) {
 	console.log("API KEY not in environment variables. Please provide API KEY.");
 	process.exit(1);
 }
+
+// API key needed to authenticate
 const baseHeaders = {
 	"x-api-key": process.env.FH_API_KEY
 };
 
+// Make requests to the API and S3
 axios.get(`${baseURL}/signed-url-upload/${fileName}`, {
 	headers: {
 		...baseHeaders
@@ -55,11 +59,10 @@ axios.get(`${baseURL}/signed-url-upload/${fileName}`, {
 }).then(res => {
 	const uploadURL = res.data.UploadURL
 	const objectKey = res.data.Key
-
+	// Calculate mime/type of the file
 	contentType = mime.getType(fileName.slice(fileName.lastIndexOf(".") + 1));
 	axios.put(uploadURL, binaryFile, {
 		headers: {
-			...baseHeaders,
 			'Content-Type': contentType
 		}
 	}).then(res => {
@@ -83,4 +86,3 @@ axios.get(`${baseURL}/signed-url-upload/${fileName}`, {
 		}
 	});
 });
-
